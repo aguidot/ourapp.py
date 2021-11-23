37d1483ae418af6f50d64b41de554ac368632551
aguidot <85652410+aguidot@users.noreply.github.com>
2021-11-23 02:29:48 -0600

    Update ourapp.py

--- PRJ3/ourapp.py
+++ PRJ3/ourapp.py
@@ -9,21 +9,23 @@ st.image('https://image.cnbcfm.com/api/v1/image/106962967-1634709558798-gettyima
 st.markdown("### Smart Risk Movements")
 st.markdown("This application assists client portfolio management by calculating transaction fees associated with transferring between Ethereum and a fund before enabling the client to initiate the transfer. The application then automates the transfer of the amount between Etherum and a fund.")
 
-mkt_account_input = st.text_input("Enter your Market Account Address:")
-st.write(mkt_account_input)
-
-mkt_account_input = st.text_input("Enter your Private Key:", type="password")
-
-
-options = st.selectbox("Which account would you like to transfer from?",
+with st.form("my_form"):
+        mkt_account_input = st.text_input("Enter your Market Account Address:")
+        st.write(mkt_account_input)
+        
+        private_key_input = st.text_input("Enter your Private Key:", type="password")
+        
+        options = st.selectbox("Which account would you like to transfer from?",
         ["Market Account", "Eth Account"])
-
-st.markdown("The fee associated with this transaction is 21,000 GWEI.")
-
-fee_agreement = st.selectbox("Would you like to proceed with transaction?",
+        
+        st.markdown("The fee associated with this transaction is 21,000 GWEI.")
+        
+        fee_agreement = st.selectbox("Would you like to proceed with transaction?",
         ["Yes", "No"])
+        
+        if submitted:
+                st.write("Market Account", mkt_account_input, "Private Key", private_key_input, "Account", options, "Fee Agreement", fee_agreement)
 
-path = st.text_input('https://github.com/aguidot/ourapp.py.git')
-if path:
-        df = pd.read_csv(path)
+st.write("Outside form")
 
+form.form_submit_button("Submit")
